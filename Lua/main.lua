bb0b5aa0c24a3bb9712fb548b9d8654b8bd29ec1
Patric Mendonça <patricmendonca@gmail.com>
2019-10-11 16:40:14 -0300

    Create main.lua

---
+++ Lua/main.lua
@@ -0,0 +1,9 @@
+print("Insira o primeiro cateto:")
+a = tonumber(io.read())
+
+print("Insira o segundo cateto: ")
+b = tonumber(io.read())
+
+
+print("\nHello HacktoberFest!")
+print("Hipotenusa: ",(math.sqrt((a * a) + (b * b))))
