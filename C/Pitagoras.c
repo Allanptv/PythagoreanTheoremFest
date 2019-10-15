c7e1b808591eb17baa9078b568ae7645b143ef7c
Henrique Fernandes <henrique.fquick@gmail.com>
2019-10-14 22:38:06 -0300

    Add files via upload

---
+++ C/Pitagoras.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    printf("Hello HacktoberFest!\n");
+    printf("Digite o valor do primeiro cateto: ");
+    int cat1;
+    scanf("%d", &cat1);
+    printf("Digite o valor do segundo cateto: ");
+    int cat2;
+    scanf("%d", &cat2);
+
+    int hip = sqrt((cat1*cat1)+(cat2*cat2));
+    printf("O valor da hipotenusa eh: %d", hip);
+
+}
