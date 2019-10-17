5e6ea4ca303770b7fbc7b098498365feaea43b82
AbhinavKrishna26 <55917035+AbhinavKrishna26@users.noreply.github.com>
2019-10-17 14:28:58 +0530

    Update Pitagoras.c

--- C/Pitagoras.c
+++ C/Pitagoras.c
@@ -9,6 +9,7 @@ void main(){
     printf("Digite o valor do segundo cateto: ");
     int cat2;
     scanf("%d", &cat2);
+    printf("hello");
 
     int hip = sqrt((cat1*cat1)+(cat2*cat2));
     printf("O valor da hipotenusa eh: %d", hip);
